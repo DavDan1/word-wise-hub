15a98e3737725526531c16a998024df04291ce25
Davit Danielyan <davit-danielyan@hotmail.com>
2024-01-18 19:36:05 +0100

    Add contentToDisplay

--- word-wise-hub-forontend/src/components/Card.tsx
+++ word-wise-hub-forontend/src/components/Card.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
+import { CardModel } from '../services/models';
 import '../style/Card.css';
 
 interface CardProps {
-  word: string;
+  card: CardModel;
+  onEdit: (card: CardModel) => void;
+  onDelete: (id: number) => void;
 }
 
-const Card: React.FC<CardProps> = ({ word }) => {
+const CardComponent: React.FC<CardProps> = ({ card, onEdit, onDelete }) => {
+  const contentToDisplay = card.isEditing ? card.editedWord : card.question;
+
   return (
     <div className="cards-ctn">
       <div className="word-ctn">
-        <button className="edit-btn">Edit</button>
-        <div>{word}</div>
-        <button className="delete-btn">Delete</button>
+        <button className="edit-btn" onClick={() => onEdit(card)}>
+          Edit
+        </button>
+        <div>{contentToDisplay}</div>
+        <button className="delete-btn" onClick={() => onDelete(card.id)}>
+          Delete
+        </button>
       </div>
     </div>
   );
 };
 
-export default Card;
+export default CardComponent;
