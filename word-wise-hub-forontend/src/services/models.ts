ada57401c915c3a4b7b27da5e2de0a1d58b433a5
Davit Danielyan <davit-danielyan@hotmail.com>
2024-01-18 19:37:31 +0100

    Add interface model

---
+++ word-wise-hub-forontend/src/services/models.ts
@@ -0,0 +1,11 @@
+export interface CardModel {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  editedWord: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  isEditing: any;
+  id: number;
+  question: string;
+  answer: string;
+  category: string;
+  definition: string;
+}
