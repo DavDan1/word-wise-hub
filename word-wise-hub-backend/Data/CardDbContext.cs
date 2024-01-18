d7c5c2dad03710c93f966d70a5ff1dd9cd9c7ff2
Davit Danielyan <davit-danielyan@hotmail.com>
2024-01-18 08:17:00 +0100

    Add DbContext

---
+++ word-wise-hub-backend/Data/CardDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer.Infrastructure.Internal;
+using Model;
+
+public class CardDbContext : DbContext
+{
+  public DbSet<Card> Cards { get; set; }
+
+  public CardDbContext(DbContextOptions<CardDbContext> options) : base(options)
+  {
+  }
+
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    optionsBuilder.UseSqlServer(optionsBuilder.Options.FindExtension<SqlServerOptionsExtension>().ConnectionString, options =>
+    {
+      options.EnableRetryOnFailure();
+    });
+  }
+}
