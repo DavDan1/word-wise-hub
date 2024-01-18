15691868f8bb5341feb1b52a68e2199b81fb0ca6
Davit Danielyan <davit-danielyan@hotmail.com>
2024-01-18 08:19:35 +0100

    Add CRUD methods

--- word-wise-hub-backend/Controllers/CardController.cs
+++ word-wise-hub-backend/Controllers/CardController.cs
@@ -19,4 +19,53 @@ public class CardController : ControllerBase
   {
     return await _context.Cards.ToListAsync();
   }
+  [HttpPost]
+  public async Task<ActionResult<Card>> CreateCard(Card card)
+  {
+    _context.Cards.Add(card);
+    await _context.SaveChangesAsync();
+    return CreatedAtAction(nameof(GetAllCards), new { }, card);
+  }
+
+  [HttpPut("{id}")]
+  public async Task<IActionResult> UpdateCard(int id, Card card)
+  {
+    if (id != card.Id)
+    {
+      return BadRequest();
+    }
+
+    _context.Entry(card).State = EntityState.Modified;
+
+    try
+    {
+      await _context.SaveChangesAsync();
+    }
+    catch (DbUpdateConcurrencyException)
+    {
+      if (!_context.Cards.Any(c => c.Id == id))
+      {
+        return NotFound();
+      }
+      throw;
+    }
+
+    return NoContent();
+  }
+
+  [HttpDelete("{id}")]
+  public async Task<IActionResult> DeleteCard(int id)
+  {
+    var card = await _context.Cards.FindAsync(id);
+
+    if (card == null)
+    {
+      return NotFound();
+    }
+
+    _context.Cards.Remove(card);
+    await _context.SaveChangesAsync();
+
+    return NoContent();
+  }
 }
