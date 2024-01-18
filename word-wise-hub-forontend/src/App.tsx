4ce0f24dac9e6bbbf2268dc0620a52ea2c4787fa
Davit Danielyan <davit-danielyan@hotmail.com>
2024-01-18 09:41:02 +0100

    Add MainPage to App

--- word-wise-hub-forontend/src/App.tsx
+++ word-wise-hub-forontend/src/App.tsx
@@ -1,14 +1,15 @@
-import './App.css'
+import './App.css';
+import MainPage from './components/MainPage';
 
 function App() {
-
   return (
     <>
       <div>
         <h1>Word Wise Hub</h1>
+        <MainPage />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
