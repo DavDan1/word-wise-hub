cc78b6fce6f6d7d90a82bbfe2fa5aba3692a3a66
Davit Danielyan <davit-danielyan@hotmail.com>
2024-01-18 09:44:03 +0100

    Add logo component WIP-can be removed in future

---
+++ word-wise-hub-forontend/src/components/Logo.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Logo: React.FC = () => {
+  return (
+    <div className="logo-ctn">
+      <div>
+        <h2 className="username-text">Guest</h2>
+        <p className="welcome-message">Welcome back!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Logo;
