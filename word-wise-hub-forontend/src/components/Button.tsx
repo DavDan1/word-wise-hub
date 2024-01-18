43e6040d75ed4bff5cddb917e45c846b224f80a5
Davit Danielyan <davit-danielyan@hotmail.com>
2024-01-18 09:42:38 +0100

    Add button component

---
+++ word-wise-hub-forontend/src/components/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface ButtonProps {
+  onClick: () => void;
+  children: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
+  return (
+    <button onClick={onClick} className="button">
+      {children}
+    </button>
+  );
+};
+
+export default Button;
