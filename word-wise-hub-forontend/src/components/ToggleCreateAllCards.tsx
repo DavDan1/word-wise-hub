c61e99f1aa103fb6ece82a0397da191d5dda35b6
Davit Danielyan <davit-danielyan@hotmail.com>
2024-01-19 01:49:18 +0100

    Add div for text context

--- word-wise-hub-forontend/src/components/ToggleCreateAllCards.tsx
+++ word-wise-hub-forontend/src/components/ToggleCreateAllCards.tsx
@@ -91,7 +91,7 @@ const ToggleCreateAllCards: React.FC<ToggleCreateAllCardsProps> = ({
           <button onClick={handleCancel}>Cancel</button>
         </div>
       )}
-      <div className='toggle-ctn-text'>
+      <div className="toggle-ctn-text">
         <h2 className="username-text">Guest</h2>
         <p className="welcome-message">Welcome back!</p>
       </div>
