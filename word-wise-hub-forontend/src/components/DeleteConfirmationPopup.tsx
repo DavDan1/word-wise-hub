7962d557823d7f02a759f9fe65d24c044d5db286
Davit Danielyan <davit-danielyan@hotmail.com>
2024-01-19 11:08:43 +0100

    Change the button styles Add toaster notifications

--- word-wise-hub-forontend/src/components/DeleteConfirmationPopup.tsx
+++ word-wise-hub-forontend/src/components/DeleteConfirmationPopup.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { toast } from 'react-hot-toast';
+import '../style/DeleteConfirmationPopup.css';
 
 interface DeleteConfirmationPopupProps {
   onCancel: () => void;
@@ -9,11 +11,29 @@ const DeleteConfirmationPopup: React.FC<DeleteConfirmationPopupProps> = ({
   onCancel,
   onConfirm,
 }) => {
+  const showToast = () => {
+    toast.error('🗑️ Card successfully deleted! 🎉');
+  };
+
   return (
     <div className="delete-confirmation-popup">
       <p>Are you sure you want to delete this card?</p>
-      <button onClick={onConfirm}>Yes</button>
-      <button onClick={onCancel}>No</button>
+      <button
+        className="delete-confirmation-popup-btn"
+        onClick={() => {
+          onConfirm();
+          showToast();
+        }}
+      >
+        Yes
+      </button>
+      <button
+        className="delete-confirmation-popup-btn"
+        style={{ backgroundColor: '#5db075', color: '#ffffff' }}
+        onClick={onCancel}
+      >
+        No
+      </button>
     </div>
   );
 };
