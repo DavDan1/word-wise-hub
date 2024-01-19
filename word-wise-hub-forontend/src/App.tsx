9fc308c9ad97d0d54e867a4fa497f46f1def5c1c
Davit Danielyan <davit-danielyan@hotmail.com>
2024-01-19 11:07:49 +0100

    Add toaster to the main page

--- word-wise-hub-forontend/src/App.tsx
+++ word-wise-hub-forontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from 'react-hot-toast';
 import './App.css';
 import Footer from './components/Footer';
 import MainPage from './components/MainPage';
@@ -7,6 +8,7 @@ function App() {
     <>
       <div>
         <MainPage />
+        <Toaster />
         <Footer />
       </div>
     </>
